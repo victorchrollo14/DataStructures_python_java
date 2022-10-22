1a2dbdd5aae65ca934a7154c0253037d300ca2fe
victor <chrollolucilfer@gmail.com>
2022-10-22 09:07:50 +0530

    renamed easy1.py to easy1_my.py and added easy1_op.py - which is an optimised solution for the array question

--- python_data/Arrays/easy1_my.py
+++ python_data/Arrays/easy1_my.py
@@ -1,4 +1,5 @@
 import array
+import math
 
 
 # sum of subarray
@@ -14,41 +15,58 @@ class Solution:
         for i in range(n):
             sum = sum + arr[i]
             subarray.append(arr[i])
+            print(f'sum:{sum}')
             if (sum == s):
                 print(subarray)
                 return arr.index(subarray[0]) + 1, arr.index(subarray[len(subarray) - 1]) + 1
+
+        for i in range(n):
             for j in range(i+1, n):
                 sum1 += arr[j]
                 subarray1.append(arr[j])
+                print(f'sum1: {sum1}')
                 if(sum1 == s):
                     print(subarray1)
                     k = 1
                     return arr.index(subarray1[0]) + 1, arr.index(subarray1[len(subarray1) - 1]) + 1
-        
+            print()
+            sum1 = 0
             subarray1.clear()
             if(k == 1):
                 break
-                
+        return -1, 
 
-               
 
-n = 5
-s = 12
-arr = array.array('i', [1,2,3,7,5])
+# driver code               
+def main():
+    T = int(input())
+    while(T > 0):
 
-obj = Solution()
-obj.subArraySum(arr, n, s)
-ans = obj.subArraySum(arr, n, s)
-for i in ans:
-    print(i, end=" ")
-print('\n')
+        NS = input().strip().split()
+        N = int(NS[0])
+        S = int(NS[1])
 
-n1 = 10
-s1 = 15
-arr1 = array.array('i', [1,2,3,4,5,6,7,8,9,10])
+        A = list(map(int, input().split()))
+        ob = Solution()
+        ans = ob.subArraySum(A, N, S)
+        print(ans)
+        for i in ans:
+            print(i, end=" ")
+        
+        print()
 
-ob2 = Solution()
-ans2 = ob2.subArraySum(arr1, n1, s1)
-for i in ans2:
-    print(i, end=" ")
+        T -= 1
+
+if __name__ == "__main__":
+    main()
+
+
+
+# A = list(map(int, input().split()))
+# ob = Solution()
+# ans = ob.subArraySum(A, 42, 468)
+# for i in ans:
+#     print(i, end=" ")
+
+
 
